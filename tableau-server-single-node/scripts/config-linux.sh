3c68aa72522e226c5450821b8916bc021c3a57fd
Tom Gallivan <57298907+sideshowtom@users.noreply.github.com>
2020-11-08 08:40:34 -0500

    Update config-linux.sh

--- tableau-server-single-node/scripts/config-linux.sh
+++ tableau-server-single-node/scripts/config-linux.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 while getopts "u:p:f:g:h:i:j:k:l:m:n:o:q:r:s:t:v:w:x:y:" option
 do
  case "${option}"
@@ -79,100 +78,6 @@ fi
 
 # download automated-installer
 wget --remote-encoding=UTF-8 --output-document=automated-installer.sh $INSTALL_SCRIPT_URL
-
-wait
-chmod +x automated-installer.sh
-
-
-echo "USER=" >>installer_log.txt
-echo $USER >>installer_log.txt
-echo "OS=" >>installer_log.txt
-echo $OS >>installer_log.txt
-echo "TS_USER=" >>installer_log.txt
-echo $TS_USER >>installer_log.txt
- 
-echo "modified automated-installer" >> installer_log.txt
-
-# ensure everything is finished
-wait
-
-# run automated installer (install trial if no license key)
-if [ -z "$LICENSE_KEY" ]
-then
-  if [ "$OS" == "Ubuntu 16.04 LTS" ]
-  then
-    sudo ./automated-installer.sh -s secrets -f config.json -r registration.json -a "$USER" --accepteula tableau-installer.deb --force
-  else
-    sudo ./automated-installer.sh -s secrets -f config.json -r registration.json -a "$USER" --accepteula tableau-installer.rpm --force
-  fi
-else
-  if [ "$OS" == "Ubuntu 16.04 LTS" ]
-  then
-    sudo ./automated-installer.sh -s secrets -f config.json -r registration.json -a "$USER" -k "$LICENSE_KEY" --accepteula tableau-installer.deb --force
-  else
-    sudo ./automated-installer.sh -s secrets -f config.json -r registration.json -a "$USER" -k "$LICENSE_KEY" --accepteula tableau-installer.rpm --force
-  fi
-fi
-
-wait
-
-# if on RHEL, open firewall
-if [ "$OS" == "RHEL 7.6" ] || [ "$OS" == "CentOS 7.5" ]
-then
-  firewall-cmd --zone=public --add-port=80/tcp --permanent
-  firewall-cmd --reload
-fi
-
-# remove all install files
-rm registration.json
-rm secrets
-if [ "$OS" == "RHEL 7.6" ] || [ "$OS" == "CentOS 7.5" ]
-then
-  rm tableau-installer.rpm
-else
-  rm tableau-installer.deb
-fi
-rm automated-installer.sh
-rm config.json
-
- \"eula\" : \"$EULA\",
- \"title\" : \"$TITLE\",
- \"phone\" : \"$PHONE\",
- \"company\" : \"$COMPANY-azure-arm-linux\",
- \"state\" : \"$STATE\",
- \"department\" : \"$DEPARMENT\",
- \"first_name\" : \"$FIRST_NAME\",
- \"email\" : \"$EMAIL\"
-}" >> registration.json
-
-# create config file
-echo '{
-  "configEntities": {
-    "identityStore": {
-      "_type": "identityStoreType",
-      "type": "local"
-    }
-  }
-}' >> config.json
-wait
-
-# download tableau server .deb or.rpm file
-# retry on fail
-if [ "$OS" == "Ubuntu 16.04 LTS" ]
-then
-  wget --tries=3 --output-document=tableau-installer.deb https://downloads.tableau.com/esdalt/2020.1.1/tableau-server-2020-1-1_amd64.deb
-else
-  wget --tries=3 --output-document=tableau-installer.rpm https://downloads.tableau.com/esdalt/2020.1.1/tableau-server-2020-1-1.x86_64.rpm
-fi
-
-if [ $? -ne 0 ]
-then
-  echo "wget of Tableau installer failed" >> installer_log.txt
-  exit 1;
-fi
-
-# download automated-installer
-#wget --remote-encoding=UTF-8 --output-document=automated-installer.sh $INSTALL_SCRIPT_URL
 
 wait
 chmod +x automated-installer.sh
