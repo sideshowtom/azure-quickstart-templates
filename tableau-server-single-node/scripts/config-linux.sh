31b09c32c698b81aaf0d2bd206ec7e68d5bf19ed
Tom Gallivan <57298907+sideshowtom@users.noreply.github.com>
2020-11-09 10:49:22 -0500

    Update config-linux.sh

--- tableau-server-single-node/scripts/config-linux.sh
+++ tableau-server-single-node/scripts/config-linux.sh
@@ -96,7 +96,7 @@ wait
 #sudo dpkg-reconfigure debconf -f noninteractive -p critical
 #export DEBIAN_FRONTEND=noninteractive
 
-echo "Dpkg::Options {'--force-confdef'; '--force-confold';}" >> /etc/apt/apt.conf.d/local
+echo "Dpkg::Options {'--force-confnew';}" >> /etc/apt/apt.conf.d/local
 
 # run automated installer (install trial if no license key)
 if [ -z "$LICENSE_KEY" ]
